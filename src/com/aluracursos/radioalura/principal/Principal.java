00d31422aca78f00218907f7421ebcd82c0f6d50
developerharo <developerharo@gmail.com>
2024-11-09 18:40:57 -0600

    Agregando archivos del proyecto radioalura

---
+++ src/com/aluracursos/radioalura/principal/Principal.java
@@ -0,0 +1,39 @@
+package com.aluracursos.radioalura.principal;
+
+import com.aluracursos.radioalura.modelos.Cancion;
+import com.aluracursos.radioalura.modelos.MisFavoritos;
+import com.aluracursos.radioalura.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Cancion miCancion = new Cancion();
+        miCancion.setTitulo("Forever");
+        miCancion.setCantante("Kiss");
+
+        Podcast miPodcast = new Podcast();
+        miPodcast.setPresentador("Yordi Rosado");
+        miPodcast.setTitulo("Cafe.Tech");
+        // canción
+        for (int i = 0; i < 100; i++) {
+            miCancion.meGusta();
+        }
+        for (int i = 0; i < 2000; i++) {
+            miCancion.reproduce();
+        }
+        // podccast
+        for (int i = 0; i < 100; i++) {
+            miPodcast.meGusta();
+        }
+
+        for (int i = 0; i < 8000; i++) {
+            miPodcast.reproduce();
+        }
+
+        System.out.println("Total reproducciones: " + miCancion.getTotalDeReproducciones());
+        System.out.println("Total me gusta: " + miCancion.getTotalDeMeGusta());
+
+        MisFavoritos favoritos = new MisFavoritos();
+        favoritos.adicione(miPodcast);
+        favoritos.adicione(miCancion);
+    }
+}
